e34439ef3b5abacf3de3345205538c0351af7a1e
Ilya Vylegzhanin <vylegzhanin.ilja@yandex.ru>
2022-12-22 19:49:47 +0300

    feat: add styles to store-page

--- src/components/cards.ts
+++ src/components/cards.ts
@@ -1,5 +1,7 @@
 import data from './data-base.js';
 import { Data } from './data-base.js';
+import './store';
+
 
 // class Card {
 //     id: number;
@@ -31,33 +33,35 @@ import { Data } from './data-base.js';
 
 function createCard(obj: Data) {
     const cardElem = document.createElement('div');
+    cardElem.classList.add('card')
+    cardElem.style.background = `URL(${obj.images[0]}) 0 0 /cover`
     const cardName = document.createElement('div');
+    cardName.classList.add('card__title')
     const infoElem = document.createElement('div');
+    infoElem.classList.add('card__info')
+    const cardBtns = document.createElement('div')
+    cardBtns.classList.add('card__btns')
     const addBtn = document.createElement('button');
     const detailsBtn = document.createElement('button');
+    cardBtns.appendChild(addBtn)
+    cardBtns.appendChild(detailsBtn)
 
     cardName.innerText = obj.title
-    infoElem.innerText = `${obj.category}\n${obj.brand}\n${obj.price}$\n
-                            ${obj.discountPercentage}%\n${obj.rating}\n${obj.stock}\n`
+    infoElem.innerText = `Category: ${obj.category}\nBrand: ${obj.brand}\nPrice: ${obj.price}$
+                            Discount: ${obj.discountPercentage}%\nRating: ${obj.rating}\nStock: ${obj.stock}`
     addBtn.innerText = 'ADD TO CART'
     detailsBtn.innerText = 'DETAILS'
     cardElem.appendChild(cardName)
     cardElem.appendChild(infoElem)
-    cardElem.appendChild(addBtn)
-    cardElem.appendChild(detailsBtn)
+    cardElem.appendChild(cardBtns)
     return cardElem
 }
 
 function appendCards(data: Data[]) {
     data.forEach((obj) => {
         let cardElem = createCard(obj);
-        console.log(cardElem);
-
-        let block = document.querySelector('.store__container');
-        console.log(block);
-
+        let block = document.querySelector('.products__items');
         block?.appendChild(cardElem)
-    })
+    });
 }
-
 export default appendCards
---
+++ src/components/store.ts
@@ -0,0 +1,27 @@
+// const SMALL_VIEW = document.querySelector('.view-mode__small') as HTMLDivElement
+// const BIG_VIEW = document.querySelector('.view-mode__big') as HTMLDivElement
+// const ITEMS_LIST = document.querySelector('.products__items') as HTMLDivElement
+
+// function addListeners() {
+//     console.log(ITEMS_LIST);
+//     SMALL_VIEW.addEventListener('click', toSmallView);
+//     BIG_VIEW.addEventListener('click', toBigView)
+// }
+
+
+
+// function toSmallView() {
+//     if (ITEMS_LIST?.classList.contains('_resize-products')) {
+//         return
+//     }
+//     (ITEMS_LIST as HTMLDivElement).classList.toggle('_resize-products')
+
+// }
+// function toBigView() {
+//     if (!ITEMS_LIST?.classList.contains('_resize-products')) {
+//         return
+//     }
+//     (ITEMS_LIST as HTMLDivElement).classList.toggle('_resize-products')
+// }
+
+// export default addListeners
