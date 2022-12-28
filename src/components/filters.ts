aba9c465b2235e25675767d0973896ee080ad8f5
Vylegzhanin Ilya <vylegzhanin.ilja@yandex.ru>
2022-12-29 00:11:34 +0300

    feat: add dual range slider

--- src/components/filters.ts
+++ src/components/filters.ts
@@ -5,12 +5,20 @@ class Filters {
     BRAND_ELEM: HTMLDivElement;
     PRICE_ELEM: HTMLDivElement;
     STOCK_ELEM: HTMLDivElement;
+    fromSlider: HTMLInputElement;
+    toSlider: HTMLInputElement;
+    fromInput: HTMLInputElement;
+    toInput: HTMLInputElement;
     constructor(data: Data[]) {
         this.data = data;
         this.CATEGORY_ELEM = document.querySelector("[filtername='category'] .filters__list") as HTMLDivElement
         this.BRAND_ELEM = document.querySelector("[filtername='brand'] .filters__list") as HTMLDivElement
         this.PRICE_ELEM = document.querySelector("[filtername='category'] .filters__list") as HTMLDivElement
         this.STOCK_ELEM = document.querySelector("[filtername='brand'] .filters__list") as HTMLDivElement
+        this.fromSlider = document.querySelector('#fromSlider') as HTMLInputElement;
+        this.toSlider = document.querySelector('#toSlider') as HTMLInputElement;
+        this.fromInput = document.querySelector('#fromInput') as HTMLInputElement;
+        this.toInput = document.querySelector('#toInput') as HTMLInputElement;
     }
     _addFilters() {
         let categoryArr = Array.from(new Set(this.data.map((item) => item.category)));
@@ -26,7 +34,7 @@ class Filters {
                 labelForInput.innerText = item
                 const qntElem = document.createElement('span')
                 const quontity = data.filter((x) => (arr === categoryArr ? x.category : x.brand) === item).length
-                qntElem.innerText = `${quontity}/${quontity}`
+                qntElem.innerText = ` (${quontity}/${quontity})`
                 newCheckboxElem.appendChild(newInputElem)
                 newCheckboxElem.appendChild(labelForInput)
                 newCheckboxElem.appendChild(qntElem)
@@ -35,7 +43,71 @@ class Filters {
         }
         appendCheckboxes(this.data, categoryArr, this.CATEGORY_ELEM)
         appendCheckboxes(this.data, brandArr, this.BRAND_ELEM)
+        // ----dual-range------
+        function controlFromInput(fromSlider: HTMLInputElement, fromInput: HTMLInputElement, toInput: HTMLInputElement, controlSlider: HTMLInputElement) {
+            const [from, to] = getParsed(fromInput, toInput);
+            if (from > to) {
+                fromSlider.value = String(to);
+                fromInput.value = String(to);
+            } else {
+                fromSlider.value = String(from);
+            }
+        }
 
+        function controlToInput(toSlider: HTMLInputElement, fromInput: HTMLInputElement, toInput: HTMLInputElement, controlSlider: HTMLInputElement) {
+            const [from, to] = getParsed(fromInput, toInput);
+            setToggleAccessible(toInput);
+            if (from <= to) {
+                toSlider.value = String(to);
+                toInput.value = String(to);
+            } else {
+                toInput.value = String(from);
+            }
+        }
+
+        function controlFromSlider(fromSlider: HTMLInputElement, toSlider: HTMLInputElement, fromInput: HTMLInputElement) {
+            const [from, to] = getParsed(fromSlider, toSlider);
+            if (from > to) {
+                fromSlider.value = String(to);
+                fromInput.value = String(to);
+            } else {
+                fromInput.value = String(from);
+            }
+        }
+
+        function controlToSlider(fromSlider: HTMLInputElement, toSlider: HTMLInputElement, toInput: HTMLInputElement) {
+            const [from, to] = getParsed(fromSlider, toSlider);
+            setToggleAccessible(toSlider);
+            if (from <= to) {
+                toSlider.value = String(to);
+                toInput.value = String(to);
+            } else {
+                toInput.value = String(from);
+                toSlider.value = String(from);
+            }
+        }
+
+        function getParsed(currentFrom: { value: string; }, currentTo: { value: string; }) {
+            const from = parseInt(currentFrom.value, 10);
+            const to = parseInt(currentTo.value, 10);
+            return [from, to];
+        }
+
+        function setToggleAccessible(currentTarget: HTMLInputElement) {
+            const toSlider = document.querySelector('#toSlider') as HTMLInputElement;
+            if (Number(currentTarget.value) <= 0) {
+                toSlider.style.zIndex = '2';
+            } else {
+                toSlider.style.zIndex = '0';
+            }
+        }
+
+        setToggleAccessible(this.toSlider);
+
+        this.fromSlider.oninput = () => controlFromSlider(this.fromSlider, this.toSlider, this.fromInput);
+        this.toSlider.oninput = () => controlToSlider(this.fromSlider, this.toSlider, this.toInput);
+        this.fromInput.oninput = () => controlFromInput(this.fromSlider, this.fromInput, this.toInput, this.toSlider);
+        this.toInput.oninput = () => controlToInput(this.toSlider, this.fromInput, this.toInput, this.toSlider);
     }
 }
 
