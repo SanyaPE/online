765469bf7e765681b3e864d965d088844d29e164
Alexander Pepler <pepleralex@gmail.com>
2023-01-09 23:38:46 +0300

    fix: clear branch main

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common.js');
-
-module.exports = (env, argv) => {
-    const buildMode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
-    const modeConfig = require(`./webpack.${buildMode}.js`);
-    return merge(commonConfig, modeConfig);
-};
--- webpack.production.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-
-module.exports =  {
-    mode: 'production',
-    devtool: 'source-map',
-};
